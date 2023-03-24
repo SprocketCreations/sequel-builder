ee1421f05825b9e8c279cc0aa191f791a4e3dc28
Kailen James <sevenflags57@gmail.com>
2023-03-24 12:09:10 -0700

    Removed old Not classes from index

--- src/lib/expressionTypes/index.ts
+++ src/lib/expressionTypes/index.ts
@@ -5,22 +5,14 @@ import Xor from "./Xor";
 export { Xor };
 import And from "./And";
 export { And };
-import Not from "./Not";
-export { Not };
 import Is from "./Is";
 export { Is };
-import IsNot from "./IsNot";
-export { IsNot };
 import IsUnknown from "./IsUnknown";
 export { IsUnknown };
-import IsNotUnknown from "./IsNotUnknown";
-export { IsNotUnknown };
 
 // Boolean Primary
 import IsNull from "./IsNull";
 export { IsNull };
-import IsNotNull from "./IsNotNull";
-export { IsNotNull };
 import Spaceship from "./Spaceship";
 export { Spaceship };
 import Compare from "./Compare";
@@ -35,28 +27,16 @@ import InQuery from "./InQuery";
 export { InQuery };
 import InExpression from "./InExpression";
 export { InExpression };
-import NotInQuery from "./NotInQuery";
-export { NotInQuery };
-import NotInExpression from "./NotInExpression";
-export { NotInExpression };
 import Between from "./Between";
 export { Between };
-import NotBetween from "./NotBetween";
-export { NotBetween };
 import SoundsLike from "./SoundsLike";
 export { SoundsLike };
 import Like from "./Like";
 export { Like };
-import NotLike from "./NotLike";
-export { NotLike };
 import RegexExpression from "./RegexExpression";
 export { RegexExpression };
 import Regex from "./Regex";
 export { Regex };
-import NotRegexExpression from "./NotRegexExpression";
-export { NotRegexExpression };
-import NotRegex from "./NotRegex";
-export { NotRegex };
 
 // BitExpr
 import BitOr from "./BitOr";
@@ -91,30 +71,20 @@ const expressionTypes = {
 	Or,
 	Xor,
 	And,
-	Not,
 	Is,
-	IsNot,
 	IsUnknown,
-	IsNotUnknown,
 	IsNull,
-	IsNotNull,
 	Spaceship,
 	Compare,
 	CompareAll,
 	CompareAny,
 	InQuery,
 	InExpression,
-	NotInQuery,
-	NotInExpression,
 	Between,
-	NotBetween,
 	SoundsLike,
 	Like,
-	NotLike,
 	RegexExpression,
 	Regex,
-	NotRegexExpression,
-	NotRegex,
 	BitOr,
 	BitAnd,
 	BitXor,
